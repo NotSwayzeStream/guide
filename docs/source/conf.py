cd7af94a6d4c9e17e1d72dcf54fdeb1edbfc585b
NotSwayze <153142943+NotSwayze@users.noreply.github.com>
2024-07-29 22:05:47 -0700

    Cleanup: Tutorial Files (#1)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
-
-release = '0.1'
-version = '0.1.0'
+project = 'NotSwayze\'s Guide to Everything'
+copyright = '2024, NotSwayze'
+author = 'NotSwayze'
 
 # -- General configuration
 
